e863a4dac2da5214d49238ae314aeac5584b103e
JeongSoo Doo <jsd@dhcp-128-189-205-21.ubcsecure.wireless.ubc.ca>
2019-10-07 13:43:36 -0700

    use 2 spaces

--- sokoban.hs
+++ sokoban.hs
@@ -3,12 +3,12 @@
 -- :load sokoban
 
 data State = State Board
-	deriving (Ord, Eq, Show)
+  deriving (Ord, Eq, Show)
 
 type Board = [[Char]]
 
 data Result = EndOfGame Char State
-	| ContinueGame State
+  | ContinueGame State
 
 -- takes current state, inputted move, and returns next state
 type Game = State -> Char -> Result
