dbae8473aa415ba4a4ec8beb362dacb1ff40d434
JeongSoo Doo <jsd@dhcp-128-189-205-21.ubcsecure.wireless.ubc.ca>
2019-10-07 13:41:28 -0700

    initial

---
+++ sokoban.hs
@@ -0,0 +1,16 @@
+-- To run 
+-- ghci
+-- :load sokoban
+
+data State = State Board
+	deriving (Ord, Eq, Show)
+
+type Board = [[Char]]
+
+data Result = EndOfGame Char State
+	| ContinueGame State
+
+-- takes current state, inputted move, and returns next state
+type Game = State -> Char -> Result
+
+
